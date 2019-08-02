fded142a492162274b9530b4a34b9898924b5de9
MinusGix <MinusGix@gmail.com>
2019-08-01 21:08:11 -0500

    Change style from class to functions and update readme

--- README.md
+++ README.md
@@ -2,22 +2,35 @@
 A really simple imgur uploader, for when you don't need that much. Does not use any libraries that aren't included in nodejs (`https`, and `querystring`).  
 Create a new instance of the module, give it a client id, and then tell it to upload a buffer you give it. Calls the callback you gave it (there is a Promise version with async at the end) with the (err, data) from imgur. Data is an object (parsed from imgur response json), or null if err isn't null. If err isn't null, then, you have an error.  
 Does not support uploading to a user account, and don't plan to.  
-Example code:  
+Goto this url to register an application with imgur: https://api.imgur.com/oauth2/addclient I believe you'll need an account on imgur.  
+Example code showcasing basically all the api:  
 ```js
 let imgur = require("../ImgurUploader/index.js");
 
-let instance = new imgur("your-client-id here");
+let clientID = "your-client-id";
 
 let file = require('fs').readFileSync("./bool.jpg");
 
-instance.uploadFileBuffer(file, (err, data) => {
+imgur.uploadFileBuffer(clientID, file, (err, data) => {
     if (err) {
         throw err;
     }
     console.log(data);
 }, "image");
 
-instance.uploadFileBufferAsync(file, "image")
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+imgur.uploadFileBufferAsync(clientID, file, "image")
+    .then(data => console.log("Async", data))
+    .catch(err => console.error("Asyncerr", err));
 ```
+The methods have some extra parameters for things like title, description, filename (not sure what this does on imgur), and a few others that are just passed in the request.  
+These todos are just thing that'd be nice to have, currently it's in an 'okay' state since it's mainly meant to be an uploader.
+TODO:  
+- Add ability to delete files with the deletehash  
+- Add ability to add it to an album (the docs say this can be done for anonymous albums if you use the deletehash)  
+- Add ability to set/unset disable_audio flag in the request  
+- Add ability to get image data  
+- Add ability to update image info (you can use deletehash)  
+- Get info about album  
+- Get album images  
+- Get a specific album image  
+- ... basically just go through the list and hopefully support most things which don't require authentication
--- index.js
+++ index.js
@@ -1,72 +1,70 @@
 const https = require('https');
 const qs = require('querystring');
 
-class ImgurUploader {
-    constructor (clientID=null) {
-        this.clientID = clientID;
-        this.api = "api.imgur.com";
-        this.apiPath = "/3/upload/";
-    }
+module.exports = {
+    api: "api.imgur.com",
+    apiPath: "/3/upload/",
 
-    uploadFileBuffer (buffer, callback=((err,data)=>{}), type="image", title="Autuploaded", description="", filename="file") {
-        let data = {
-            type: "base64",
-            title: title,
-            description: description,
-            name: filename,
-        };
-
-        if (type === "image") {
-            data.image = buffer.toString('base64');
-        } else if (type === "video") {
-            data.video = buffer.toString('base64');
-        } else {
-            return false;
-        }
-
-        data = qs.stringify(data);
-
-
-
-        let req = https.request({
-            headers: {
-                "Authorization": "Client-ID " + this.clientID,
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': Buffer.byteLength(data)
-            },
-            method: "POST",
-            hostname: this.api,
-            path: this.apiPath,
-        }, (res) => {
-            let output = '';
-            res.on('data', d => {
-                output += d;
-            });
-            res.on("end", () => {
-                callback(null, JSON.parse(output));
-            });
-        });
-
-        req.on("error", err => {
-            callback(err, null);
-        });
-
-        req.write(data);
-        req.end();
-    }
-
-    async uploadFileBufferAsync (buffer, type="image", title="Autuploaded", description="", filename="file") {
-        var self = this;
-        return new Promise(function (resolve, reject) {
-            self.uploadFileBuffer(buffer, function (err, data) {
-                if (err !== null) {
-                    reject(err);
-                } else {
-                    resolve(data);
-                }
-            }, type, title, description, filename);
-        });
-    }
+    uploadFileBuffer: uploadFileBuffer,
+    uploadFileBufferAsync: uploadFileBufferAsync,
 };
 
-module.exports = ImgurUploader;
+
+function uploadFileBuffer (clientID, buffer, callback=((err,data)=>{}), type="image", title="Autuploaded", description="", filename="file") {
+    let data = {
+        type: "base64",
+        title: title,
+        description: description,
+        name: filename,
+    };
+
+    if (type === "image") {
+        data.image = buffer.toString('base64');
+    } else if (type === "video") {
+        data.video = buffer.toString('base64');
+    } else {
+        return false;
+    }
+
+    data = qs.stringify(data);
+
+
+
+    let req = https.request({
+        headers: {
+            "Authorization": "Client-ID " + clientID,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(data)
+        },
+        method: "POST",
+        hostname: module.exports.api,
+        path: module.exports.apiPath,
+    }, (res) => {
+        let output = '';
+        res.on('data', d => {
+            output += d;
+        });
+        res.on("end", () => {
+            callback(null, JSON.parse(output));
+        });
+    });
+
+    req.on("error", err => {
+        callback(err, null);
+    });
+
+    req.write(data);
+    req.end();
+}
+
+function uploadFileBufferAsync (clientID, buffer, type="image", title="Autuploaded", description="", filename="file") {
+    return new Promise(function (resolve, reject) {
+        uploadFileBuffer(clientID, buffer, function (err, data) {
+            if (err !== null) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        }, type, title, description, filename);
+    });
+}
